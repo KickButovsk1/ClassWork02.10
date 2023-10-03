5fad7be7c53d966c851c3f5d9224ca6d314af222
KickButovsk1 <144268614+KickButovsk1@users.noreply.github.com>
2023-10-03 23:48:59 +0500

    Add files via upload

---
+++ HomeWork4.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите строку");
+            string str = Console.ReadLine();
+            str = str.Trim();
+            if (str.Contains(" "))
+            {
+                str = str.Replace(" ", "");
+            }
+            int a = (str.Length);
+            int b = a/2;
+            string c = str.Substring(0, b);
+            string d = str.Substring(b);
+            string e = new string(d.Reverse().ToArray());
+            if (c == e)
+            {
+                Console.WriteLine("строка палиндром");
+            }
+            else
+            {
+                Console.WriteLine("строка не палиндром");
+            }
+            Console.ReadKey();
+        }
+    }
+    }
+    
