1d2b94a719d0ba655024905ce7280076fc836c34
KickButovsk1 <144268614+KickButovsk1@users.noreply.github.com>
2023-10-03 22:52:04 +0500

    Add files via upload

---
+++ HomeWork3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стороны прямоугольника через пробел");
+            string str = Console.ReadLine();
+            str = str.Trim();
+            while (str.Contains("  "))
+            {
+                str = str.Replace("  ", " ");
+            }
+            string[] rez;
+            rez = str.Split(' ');
+            int a = str.Length;
+            int b;
+            bool p = int.TryParse(rez[0], out b);
+            int c;
+            bool p1 = int.TryParse(rez[1], out c);
+            int x4 = b * c;
+            if (p == true && p1 == true && a <= 3)
+            {
+                Console.WriteLine("Площадь: " + x4);
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+            Console.ReadKey();
+        }
+    }
+}
